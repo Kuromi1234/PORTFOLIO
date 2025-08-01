ec4d6cb2e82f8bb981c024b134ff134f9c9ff458
Arjun <natharjun028@gmail.com>
2025-08-02 01:39:08 +0530

    feat : WelcomeScreen

---
+++ src/components/WelcomeScreen.tsx
@@ -0,0 +1,9 @@
+const WelcomeScreen = () => {
+  return (
+    <div className="min-h-screen bg-black text-fuchsia-900 flex items-center justify-center text-4xl font-semibold tracking-wide">
+       Arjun Nath
+    </div>
+  );
+};
+
+export default WelcomeScreen;
